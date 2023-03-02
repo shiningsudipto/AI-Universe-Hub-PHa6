41555df27e7b0f93d40f1e2a4ded61ffe323d02c
Sudipto Das <shiningsudipto@gmail.com>
2023-03-03 03:24:13 +0600

    see more solved

--- js/script.js
+++ js/script.js
@@ -1,55 +1,69 @@
 const fetchData = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
         .then((res) => res.json())
-        .then((data) => showData(data.data.tools));
+        .then((data) => {
+            showData(data.data.tools.slice(0, 6));
+            seeMoreButton(data.data.tools);
+        })
     toggleSpinner(true);
-}
+};
 
-const showData = data => {
-    // console.log(data);
+const showData = (data) => {
     const dataArea = document.getElementById('data-area');
-    data.forEach(singleData => {
-        console.log(singleData);
+    dataArea.innerHTML = '';
+
+    data.forEach((singleData) => {
         dataArea.innerHTML += `
         <div class="col">
-        <div class="card h-100">
+          <div class="card h-100">
             <img src="${singleData.image}" class="card-img-top" alt="...">
             <div class="card-body">
+              <div>
+                <h5 class="card-title">Features</h5>
+                <ol>
+                  <li>${singleData.features[0]}</li>
+                  <li>${singleData.features[1]}</li>
+                  <li>${singleData.features[2]}</li>
+                </ol>
+              </div>
+              <hr>
+              <div class="d-flex justify-content-between align-items-center">
                 <div>
-                    <h5 class="card-title">Features</h5>
-                    <ol>
-                        <li>${singleData.features[0]}</li>
-                        <li>${singleData.features[1]}</li>
-                        <li>${singleData.features[2]}</li>
-                    </ol>
+                  <h5 class="card-title">${singleData.name}</h5>
+                  <div>
+                    <p><i class="bi bi-calendar4-week"></i> <span id="date-time">${singleData.published_in}</span></p>
+                  </div>
                 </div>
-                <hr>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 class="card-title">${singleData.name}</h5>
-                        <div>
-                            <p><i class="bi bi-calendar4-week"></i> <span id="date-time">${singleData.published_in}</span></p>
-                        </div>
-                    </div>
-                    <div>
-                       <button type="button" id="details-btn"  data-bs-toggle="modal" data-bs-target="#exampleModal"> <i class="bi bi-arrow-right text-danger"></i></button>
-                    </div>
+                <div>
+                  <button type="button" class="details-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <i class="bi bi-arrow-right text-danger"></i>
+                  </button>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-        `
-    })
+      `;
+    });
     toggleSpinner(false);
-}
+};
 
-const toggleSpinner = isLoading => {
+const seeMoreButton = (data) => {
+    const seeMoreBtn = document.getElementById('see-more');
+
+    seeMoreBtn.addEventListener('click', () => {
+        showData(data);
+        seeMoreBtn.style.display = 'none';
+    });
+};
+
+const toggleSpinner = (isLoading) => {
     const loaderSection = document.getElementById('loader');
     if (isLoading) {
-        loaderSection.classList.remove('d-none')
+        loaderSection.classList.remove('d-none');
     } else {
-        loaderSection.classList.add('d-none')
+        loaderSection.classList.add('d-none');
     }
-}
+};
 
-fetchData()
+fetchData();
